65d2ef869ab2cb00d0a66544682c0ef6a96fd8e9
shujoshi <shubhamjoshi12@yahoo.in>
2019-03-10 13:27:09 +0530

    Updating

--- TestGit/src/main/java/AutomationPractice/TestGit/HelloWord.java
+++ TestGit/src/main/java/AutomationPractice/TestGit/HelloWord.java
@@ -3,7 +3,7 @@ package AutomationPractice.TestGit;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("How are you");
 
 	}
 
