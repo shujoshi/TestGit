e8fd46be94bee72f82e76f0842528df69fe11300
shujoshi <shubhamjoshi12@yahoo.in>
2019-03-10 23:32:16 +0530

    sccripting

--- TestGit/src/main/java/AutomationPractice/TestGit/HelloWord.java
+++ TestGit/src/main/java/AutomationPractice/TestGit/HelloWord.java
@@ -3,7 +3,7 @@ package AutomationPractice.TestGit;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("How are you");
+		System.out.println("Shubham Joshi");
 
 	}
 
